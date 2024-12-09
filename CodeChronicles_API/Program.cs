de69ffc33d0c92a06a5ccf5a538f58ef7cad5b86
Motjiang <motjiangbrilliant@gmail.com>
2024-12-10 01:30:13 +0200

    Added DbContext, ConnectionString, Category Repository, enabled CORs

--- CodeChronicles_API/Program.cs
+++ CodeChronicles_API/Program.cs
@@ -1,3 +1,8 @@
+using CodeChronicles_API.Data;
+using CodeChronicles_API.Repositories.Implementation;
+using CodeChronicles_API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add a DbContext to the container
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+// Add repository to the container
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
@@ -17,6 +32,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// enable CORs policy
+app.UseCors(options =>
+                    options.WithOrigins("*")
+                           .AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader());
 
 app.UseAuthorization();
 
