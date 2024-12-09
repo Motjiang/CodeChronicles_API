cabbfbaa4267034afa657710e7c9c4a169fe01a2
Motjiang <motjiangbrilliant@gmail.com>
2024-12-10 01:06:00 +0200

    Added models

---
+++ CodeChronicles_API/Models/Domain/ArticleImage.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeChronicles_API.Models.Domain
+{
+    public class ArticleImage
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ CodeChronicles_API/Models/Domain/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeChronicles_API.Models.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+    }
+}
